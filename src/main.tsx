9220296daa5dd36759472f7077f71844ee001f18
Mohammad Ghasemi <daghebela@gmail.com>
2025-08-10 20:05:31 +0330

    Build a responsive layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 import App from "./App.tsx";
-import { ChakraProvider } from "@chakra-ui/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
